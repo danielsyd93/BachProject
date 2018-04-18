e3533477f72ae5e6f31d2f128f700c612f4376fc
MrEwing <s153011@student.dtu.dk>
2018-04-18 13:49:25 +0200

    180418 final

--- BachProject/src/BachProject/NewModel.java
+++ BachProject/src/BachProject/NewModel.java
@@ -12,75 +12,112 @@ import ilog.cplex.IloCplex;
 public class NewModel {
 
 
-public static void solveModel(int arc, int comm, int vert, double[][] Dik, double[] ua,
-		double[][] Cak, double[] fa, double[][] dpi, double[][] dni) {
+public static void solveModel(int arc, int comm, int vert, double[][] AposSV, double[][] AnegSV, int arcS, int arcT, double[] costArcS, double[] costArcT, int vessal, 
+		double[] containersK, double[] revenueK, double[] timeA, double[] MaxTimeK, int MaxCargo, double[][] Aposv, double[][] Anegv,
+		double[] DestVertK, double[] OrigVertK) {
 	try {
 		
 		IloCplex model = new IloCplex();
 		
-		IloIntVar[][] xak = new IloIntVar[arc][comm];
-		for(int i = 0; i < arc; i++) {
-			for(int j = 0; j < comm; j++) {
-				xak[i][j] = model.intVar(0, Integer.MAX_VALUE);	
-			}
-		}
-		
+		IloIntVar[][] xka = new IloIntVar[arc][comm];
 		IloIntVar[] ya = new IloIntVar[arc];
-		for(int i = 0; i < arc; i++) {
-			ya[i] = model.intVar(0,1);
+		for(int a = 0; a < arc; a++) {
+			for(int k = 0; k < comm; k++) {
+				xka[k][a] = model.intVar(0,1);	
+			}
+			ya[a] = model.intVar(0,1);
 		}
 		
 		IloLinearNumExpr obj = model.linearNumExpr();
-		for(int i = 0; i < arc; i++) {
-			for(int j = 0; j < comm; j++) {
-				obj.addTerm(Cak[i][j], xak[i][j]);
+		for(int a = 0; a < arcS; a++) {
+			obj.addTerm(costArcS[a],ya[a]);
+		}
+		
+		for(int a = 0; a < arcT; a++) {
+			for(int k = 0; k < comm; k++) {
+				obj.addTerm(costArcS[a],xka[k][a]);
 			}
 		}
-		for(int i = 0; i < arc; i++) {
-			obj.addTerm(fa[i],ya[i]);
+		for (int k = 0; k < comm; k++) {
+			for(int v = 0; v < DestVertK.length; v++) {
+				for(int a = 0; a < Anegv[v].length; a++) {
+					double temp = (containersK[k]* revenueK[k]);
+					obj.addTerm(xka[k][a], temp);
+				}
+			}
 		}
 		
 		model.addMinimize(obj);
-		int p = 1;
-		int n = -1;
+		
+		
 		
 		List<IloRange> constraints = new ArrayList<IloRange>();
-		for(int z = 0; z < vert; z++) {
-			for(int i = 0; i < comm; i++) {
-				System.out.println(i);
-				System.out.println(dpi[z].length);
-				System.out.println(dni[z].length);
-				IloLinearNumExpr constraint = model.linearNumExpr();
-				for(int j = 0; j < dpi[z].length; j++) {
-					int parc = (int) dpi[z][j];
-					constraint.addTerm(xak[parc-1][i], p);
+		for(int k = 0; k < comm; k++) {
+			IloLinearNumExpr constraint1 = model.linearNumExpr();
+			IloLinearNumExpr constraint2 = model.linearNumExpr();
+			for(int v = 0; v < DestVertK.length; v++) {
+				for(int a = 0; a < Anegv[v].length; a++) {
+					constraint1.addTerm(xka[k][a], 1);
 				}
-				for(int j = 0; j < dni[z].length; j++) {
-					int narc = (int) dni[z][j];
-					constraint.addTerm(xak[narc-1][i], n);
+				for(int a = 0; a < Aposv[v].length; a++) {
+					constraint2.addTerm(xka[k][a], 1);
 				}
-				constraints.add(model.addEq(constraint, Dik[z][i]));
-				System.out.println(constraints);
-			}	
-		}
-		for(int i = 0; i < arc; i++) {
-			IloLinearNumExpr constraint = model.linearNumExpr();
-			for(int j = 0; j < comm; j++) {
-				constraint.addTerm(p, xak[i][j]);
 			}
-			constraints.add((IloRange) model.addGe(model.prod(ua[i], ya[i]), constraint));
+			constraints.add(model.addLe(constraint1, 1));
+			constraints.add(model.addLe(constraint2, 0));
+			for(int v = 0; v < vert; v++) {
+				IloLinearNumExpr constraint = model.linearNumExpr();
+				if(v != DestVertK[k] && v != OrigVertK[k]) {
+					for(int a = 0; a < Aposv[v].length; a++) {
+						constraint.addTerm(xka[k][a], 1);
+					}
+					for(int a = 0; a < Anegv[v].length; a++) {
+						constraint.addTerm(xka[k][a], -1);
+					}
+				}
+				constraints.add(model.addEq(constraint, 0));
+			}
+			IloLinearNumExpr constraint = model.linearNumExpr();
+			for(int a = 0; a < arc; a++) {
+				constraint.addTerm(timeA[a], xka[k][a]);
+			}
+			constraints.add(model.addLe(constraint, MaxTimeK[k]));
 		}
-
+		
+		
+		for(int v = 0; v < vert; v++) {
+			IloLinearNumExpr constraint1 = model.linearNumExpr();
+			IloLinearNumExpr constraint2 = model.linearNumExpr();
+			for(int a = 0; a < AposSV[v].length; a++) {
+				constraint1.addTerm(ya[a], 1);
+				constraint2.addTerm(ya[a], 1);
+			}
+			constraints.add(model.addLe(constraint1, 1));
+			for(int a = 0; a < AnegSV[v].length; a++) {
+				constraint2.addTerm(ya[a],-1);
+			}
+			constraints.add(model.addEq(constraint2, 0));
+		}
+		
+		for(int a = 0; a < arcS; a++) {
+			IloLinearNumExpr constraint = model.linearNumExpr();
+			for(int k = 0; k < comm; k++) {
+				constraint.addTerm(containersK[k],xka[k][a]);
+			}
+			constraints.add((IloRange) model.addLe(constraint, model.prod(MaxCargo, ya[a])));
+		}
+		
+		
 		
 		boolean isSolved = model.solve();
 		if(isSolved) {
 			double objValue = model.getObjValue();
 			System.out.println("obj_val = " 	+ objValue);
-			for(int i = 0; i < arc; i++) {
-				for(int j = 0; j < comm; j++) {
-					System.out.println("xak[" + (i+1) + "] [" + (j+1) + "] = " + model.getValue(xak[i][j]));
+			for(int a = 0; a < arc; a++) {
+				for(int k = 0; k < comm; k++) {
+					System.out.println("xka[" + (k+1) + "] [" + (a+1) + "] = " + model.getValue(xka[k][a]));
 				}
-				System.out.println("path " + (i+1) + " is " + model.getValue(ya[i]));
+				System.out.println("path " + (a+1) + " is " + model.getValue(ya[a]));
 			}
 		}
 		else {
